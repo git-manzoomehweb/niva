76f48da6eb21925f1a1f9de14715aec94a4b5427
miladui <miladrealkord@gmail.com>
2025-12-02 02:17:08 +0330

    first commit create pages web (home , list tour , list hotel , list blog)

---
+++ mobile/assets/js/nivan-core.js
@@ -0,0 +1,75 @@
+// swiper-tour
+var swiper = new Swiper(".swiper-tour", {
+    slidesPerView: 1,
+    pagination: {
+        el: ".swiper-pagination-tour",
+    },
+});
+
+var swiper = new Swiper(".swiper-tour-2", {
+    slidesPerView: 1,
+    pagination: {
+        el: ".swiper-pagination-tour-2",
+    },
+});
+var
+    swiper = new Swiper(".swiper-tour-3", {
+    slidesPerView: 1,
+    pagination: {
+        el: ".swiper-pagination-tour-3",
+    },
+});
+
+var swiper = new Swiper(".swiper-tour-4", {
+    slidesPerView: 1,
+    pagination: {
+        el: ".swiper-pagination-tour-4",
+    },
+});
+
+
+// tab-tour
+function openTab(evt, tabId) {
+    const buttons = document.getElementsByClassName("btn-tab");
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove("active");
+    }
+
+    const parents = document.getElementsByClassName("tab-content");
+    for (var i = 0; i < parents.length; i++) {
+        parents[i].classList.remove("active");
+    }
+
+    evt.currentTarget.classList.add("active");
+    document.getElementById(tabId).classList.add("active");
+}
+// tab-hotel
+function openTabHotel(evt, tabId) {
+    const buttons = document.getElementsByClassName("btn-tab-hotel");
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove("active");
+    }
+
+    const parents = document.getElementsByClassName("tab-content-hotel");
+    for (var i = 0; i < parents.length; i++) {
+        parents[i].classList.remove("active");
+    }
+
+    evt.currentTarget.classList.add("active");
+    document.getElementById(tabId).classList.add("active");
+}
+// tab-blog
+function openTabBlog(evt, tabId) {
+    const buttons = document.getElementsByClassName("btn-tab-blog");
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove("active");
+    }
+
+    const parents = document.getElementsByClassName("tab-content-blog");
+    for (var i = 0; i < parents.length; i++) {
+        parents[i].classList.remove("active");
+    }
+
+    evt.currentTarget.classList.add("active");
+    document.getElementById(tabId).classList.add("active");
+}
---
+++ web/assets/js/nivan-core.js
@@ -0,0 +1,45 @@
+// tab-tour
+function openTab(evt, tabId) {
+    const buttons = document.getElementsByClassName("btn-tab");
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove("active");
+    }
+
+    const parents = document.getElementsByClassName("tab-content");
+    for (var i = 0; i < parents.length; i++) {
+        parents[i].classList.remove("active");
+    }
+
+    evt.currentTarget.classList.add("active");
+    document.getElementById(tabId).classList.add("active");
+}
+// tab-hotel
+function openTabHotel(evt, tabId) {
+    const buttons = document.getElementsByClassName("btn-tab-hotel");
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove("active");
+    }
+
+    const parents = document.getElementsByClassName("tab-content-hotel");
+    for (var i = 0; i < parents.length; i++) {
+        parents[i].classList.remove("active");
+    }
+
+    evt.currentTarget.classList.add("active");
+    document.getElementById(tabId).classList.add("active");
+}
+// tab-blog
+function openTabBlog(evt, tabId) {
+    const buttons = document.getElementsByClassName("btn-tab-blog");
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove("active");
+    }
+
+    const parents = document.getElementsByClassName("tab-content-blog");
+    for (var i = 0; i < parents.length; i++) {
+        parents[i].classList.remove("active");
+    }
+
+    evt.currentTarget.classList.add("active");
+    document.getElementById(tabId).classList.add("active");
+}
