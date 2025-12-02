683b84c0092e5a46f907cb295a8a38cb7f0fd9c0
miladui <miladrealkord@gmail.com>
2025-12-03 02:16:19 +0330

    create pages mobile (index , tour , hotel , blog)

---
+++ tailwind.config.mobile.js
@@ -0,0 +1,32 @@
+module.exports = {
+  content: [
+    './mobile/**/*.html',       
+    './mobile/assets/js/**/*.js'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "var(--text-primary)",
+          50: "var(--text-primary-50)",
+          100: "var(--text-primary-100)",
+          200: "var(--text-primary-200)",
+          300: "var(--text-primary-300)",
+          400: "var(--text-primary-400)",
+          500: "var(--text-primary-500)",
+          600: "var(--text-primary-600)",
+          700: "var(--text-primary-700)",
+          800: "var(--text-primary-800)",
+          900: "var(--text-primary-900)",
+          950: "var(--text-primary-950)",
+        },
+        special: {
+          1: '#FF5050CC',
+          2: '#DB934B33'
+        }
+      }
+    },
+  },
+  plugins: [],
+};
+
